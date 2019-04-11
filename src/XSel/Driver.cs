f4f7ba03301373ef904c9d485b59226ee3d39163
Michael Kaufmann (Alegri) <michael_kaufmann@hugoboss.com>
2019-04-11 16:17:15 +0200

    Add support and sample for cross browser support

--- src/XSel/Driver.cs
+++ src/XSel/Driver.cs
@@ -1,11 +1,9 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace XSel
 {
@@ -17,7 +15,7 @@ namespace XSel
             var options = new ChromeOptions();
             options.AddArguments("--no-sandbox");
 
-            if (!string.IsNullOrEmpty(mobileEmulatedDevice ))
+            if (!string.IsNullOrEmpty(mobileEmulatedDevice))
             {
                 options.EnableMobileEmulation(mobileEmulatedDevice);
             }
@@ -29,6 +27,47 @@ namespace XSel
             else
             {
                 return new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
+            }
+        }
+
+        public static RemoteWebDriver GetDriver<TWebDriver>(string mobileEmulatedDevice = null) where TWebDriver : IWebDriver, new()
+        {
+            if (typeof(TWebDriver) == typeof(ChromeDriver))
+            {
+                return GetChromeDriver(mobileEmulatedDevice);
+            }
+            else
+            {
+                IWebDriver driver;
+
+                if (typeof(TWebDriver).Name == "FirefoxDriver")
+                {
+                    string path = Environment.GetEnvironmentVariable("GeckoWebDriver");
+
+                    if (string.IsNullOrWhiteSpace(path))
+                    {
+                        path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    }
+
+                    driver = (IWebDriver)Activator.CreateInstance(typeof(TWebDriver), new object[] { path });
+                }
+                else if (typeof(TWebDriver).Name == "InternetExplorerDriver")
+                {
+                    string path = Environment.GetEnvironmentVariable("IEWebDriver");
+
+                    if (string.IsNullOrWhiteSpace(path))
+                    {
+                        path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    }
+
+                    driver = (IWebDriver)Activator.CreateInstance(typeof(TWebDriver), new object[] { path });
+                }
+                else
+                {
+                    driver = new TWebDriver();
+                }
+
+                return (RemoteWebDriver)driver;
             }
         }
     }
